4721f93b31654469869ae90ca17a3357ea18137b
Soumil Kumar <soumilkumar903@gmail.com>
2024-12-11 23:01:33 +0530

    Revert "init"
    This reverts commit 830917e26ad408af9f126f4f1a0029df961f370a.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/soumil/Soumil/Programming/cpp/ws-cpp
-# Build directory: /home/soumil/Soumil/Programming/cpp/ws-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
