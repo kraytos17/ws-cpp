830917e26ad408af9f126f4f1a0029df961f370a
Soumil Kumar <soumilkumar903@gmail.com>
2024-12-11 23:00:32 +0530

    init

---
+++ build/CMakeFiles/ws-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ws-cpp.dir/src/ws.cpp.o"
+  "CMakeFiles/ws-cpp.dir/src/ws.cpp.o.d"
+  "libws-cpp.a"
+  "libws-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ws-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/soumil/Soumil/Programming/cpp/ws-cpp
+# Build directory: /home/soumil/Soumil/Programming/cpp/ws-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
